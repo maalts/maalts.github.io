c0c9f50a4fe22274186a9e967382656bb1a4d25c
Max Alt <coolmaxalt@gmail.com>
2017-10-01 23:41:35 -0400

    Set up for heroku

--- maxaltfilms/settings.py
+++ maxaltfilms/settings.py
@@ -122,4 +122,4 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../website/static')
+    os.path.join(BASE_DIR, '../website/static'))
