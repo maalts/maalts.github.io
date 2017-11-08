1450442ddc1c12fb77653c384b1a03595cd7a3a0
Max Alt <coolmaxalt@gmail.com>
2017-11-07 19:43:02 -0500

    more tweaking

--- maxaltfilms/urls.py
+++ maxaltfilms/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
@@ -23,4 +25,5 @@ urlpatterns = [
     url(r'^', include('website.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
